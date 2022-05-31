13e0fe9404265b6a25259f9df53eb67706f1c88e
oppe-open <90128634+oppe-open@users.noreply.github.com>
2022-05-31 17:48:47 -0500

    Adding comments

--- madlibs.py
+++ madlibs.py
@@ -1,6 +1,9 @@
 # String Concatenation
+# Ways to combined strings together 
 
 name = "Brent"
+
+# Combine Strings within the func. print
 
 print("Hello my name is " + name)
 print("Hello my name is {}" .format(name))
