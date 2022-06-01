94e58b694e8efd649bd121c74706506c04721513
oppe-open <90128634+oppe-open@users.noreply.github.com>
2022-05-31 19:46:05 -0500

    Finished comp_guess function

--- super_secret_#.py
+++ super_secret_#.py
@@ -13,4 +13,31 @@ def guess(x):
                 print("Sorry. You guessed to high")
     print(f"Congrats! {guess} is correct!")
 
-guess(100)
+
+def comp_guess(x):
+    play = input(f"Please think of a number between 1 and {x}. Once you have done so please type yes ->")
+    guess = "no"
+    max_num = x
+    min_num = 1
+    comp_guess = round((max_num + min_num) / 2)
+    
+    first_time = 1
+    if play == "yes":
+        while guess != "yes":
+            
+            guess = input(f"Is your number {comp_guess} | higher | lower | yes | ->").lower
+            if guess == "higher":
+                min_num = comp_guess
+                comp_guess = round((max_num + min_num) / 2)
+                
+            elif guess == "lower":
+                max_num = comp_guess
+                comp_guess = round((max_num + min_num) / 2)
+                
+    
+        
+
+        print("Whoooo I did it! That was so much fun! ")
+
+comp_guess(999)
+
